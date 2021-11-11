91b68c9608e84ff24bfbfa115341e5375e437034
victor-chukwu <okenwa1993@gmail.com>
2021-11-11 10:24:25 +0100

    Added api call

--- src/Redux/reducer/BookReducer.js
+++ src/Redux/reducer/BookReducer.js
@@ -9,12 +9,17 @@ export const BooksReducer = (state = initiaState, { type, payload }) => {
   if (type === BookActions.ADD_BOOK) {
     return {
       ...state,
-      Books: [...state.Books, payload],
+      Books: [...state.Books, { ...payload }],
     };
   }
   if (type === BookActions.REMOVE_BOOK) {
     return {
       Books: [...state.Books.filter((book) => book.id !== payload)],
+    };
+  }
+  if (type === BookActions.FETCH_BOOKS) {
+    return {
+      Books: [...state.Books, ...payload],
     };
   }
   return state;
